a8639e32fdff0a24b3699ec346a17217b7d13f43
AllineFranciely <alline_franciely@hotmail.com>
2022-09-12 17:17:06 -0300

    criação do método Post

--- back-end/routes/animes.routes.ts
+++ back-end/routes/animes.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import AnimesController from '../controllers/Animes.controller';
+import validationAnime from '../middlewares/Animes.middleware';
 
 const router = Router();
 
@@ -7,5 +8,6 @@ const animesController = new AnimesController();
 
 router.get('/animes', animesController.getAll);
 router.get('/animes/:id', animesController.getById);
+router.post('/animes/', validationAnime, animesController.create);
 
 export default router;
