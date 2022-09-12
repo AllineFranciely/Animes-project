2b14fc7150977ff95c7810550c364b88a69a9bb7
AllineFranciely <alline_franciely@hotmail.com>
2022-09-12 17:07:21 -0300

    criação do controller de getById

--- back-end/controllers/Animes.controller.ts
+++ back-end/controllers/Animes.controller.ts
@@ -9,6 +9,18 @@ class AnimesController {
         const animes = await this.animesService.getAll();
         res.status(StatusCodes.OK).json(animes);
     };
+
+    public getById = async (req: Request, res: Response) => {
+        const id = parseInt(req.params.id);
+        const anime = await this.animesService.getById(id);
+    
+        if (!anime) {
+          return res.status(StatusCodes.NOT_FOUND)
+            .json({ message: 'Anime not found!'});
+        }
+    
+        res.status(StatusCodes.OK).json(anime);
+      }
 }
 
 export default AnimesController;
