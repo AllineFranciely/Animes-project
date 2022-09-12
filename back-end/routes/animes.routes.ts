03ac7a936609c637e5da1d6926613a2fa24e6b42
AllineFranciely <alline_franciely@hotmail.com>
2022-09-12 17:08:16 -0300

    criação da rota de getById

--- back-end/routes/animes.routes.ts
+++ back-end/routes/animes.routes.ts
@@ -6,5 +6,6 @@ const router = Router();
 const animesController = new AnimesController();
 
 router.get('/animes', animesController.getAll);
+router.get('/animes/:id', animesController.getById);
 
 export default router;
